ae2ba5118223784302547ca01b6284f41d4d1c46
santibout <santibout@yahoo.com>
2021-03-18 12:14:12 -0700

    fixed pp to app

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ require("dotenv").config();
 sgMail.setApiKey(process.env.SENDGRID_ZERO_API_KEY);
 
 app.use(cors());
-pp.use(express.json({ limit: "50mb" }));
+app.use(express.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 
 app.use(
