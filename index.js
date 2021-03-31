d490e3991b95e806fc7216549a9231be3603faf0
Santibout <santibout@yahoo.com>
2021-03-30 22:27:12 -0700

    fixed cors

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ sgMail.setApiKey(process.env.SENDGRID_ZERO_API_KEY);
 app.use(express.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "https://santibout.github.io/");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
