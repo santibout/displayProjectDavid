ca4179494236d37289ed4ebbbc2ce35fcdda9a19
Santibout <santibout@yahoo.com>
2021-03-30 21:22:48 -0700

    fixed cors

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const app = express();
 
 require("dotenv").config();
 sgMail.setApiKey(process.env.SENDGRID_ZERO_API_KEY);
-
+app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 app.use(function (req, res, next) {
