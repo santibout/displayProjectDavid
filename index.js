20bbdf5cd72cd1b2ef465238494a66547bc85355
Santibout <santibout@yahoo.com>
2021-03-30 21:05:14 -0700

    add cors

--- index.js
+++ index.js
@@ -19,15 +19,14 @@ sgMail.setApiKey(process.env.SENDGRID_ZERO_API_KEY);
 
 app.use(express.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
-// app.use(function (req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept"
-//   );
-//   next();
-// });
-app.use(cors());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 app.use(
   bodyParser.urlencoded({
     limit: "20mb",
