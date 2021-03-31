00a024fb60c3de99df5d4fdf02c37b7604f0e509
Santibout <santibout@yahoo.com>
2021-03-30 22:10:09 -0700

    fixed cors

--- index.js
+++ index.js
@@ -19,14 +19,14 @@ sgMail.setApiKey(process.env.SENDGRID_ZERO_API_KEY);
 
 app.use(express.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+// app.use(function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+// });
 app.use(
   bodyParser.urlencoded({
     limit: "20mb",
@@ -34,6 +34,8 @@ app.use(
     extended: true,
   })
 );
+
+app.use(cors());
 
 app.get("/api", async (req, res) => {
   // const client = new MongoClient(uri, { useUnifiedTopology: true });
